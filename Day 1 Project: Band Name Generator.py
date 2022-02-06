dffe0ca54d1e57f5c587fb22f8e9fa3ddef6a6e7
James Noel Sacramento <36490528+jncsacramento@users.noreply.github.com>
2022-02-06 15:45:13 +0800

    Create Day 1 Project: Band Name Generator.py

---
+++ Day 1 Project: Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Hello this is Band Name Generator App")
+city = input("What city did you grew up in?\n")
+petname = input("what's the name of your pet?\n")
+print("Your Band Name is " + str(city) + " " + str(petname))
