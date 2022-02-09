96e58c79c0c59114380c5f1365bcb3b99a788ee8
James Noel Sacramento <36490528+jncsacramento@users.noreply.github.com>
2022-02-09 23:38:34 +0800

    Update Day 4 Project: Rock Paper Scissors.py

--- Day 4 Project: Rock Paper Scissors.py
+++ Day 4 Project: Rock Paper Scissors.py
@@ -27,35 +27,28 @@ scissors = '''
 ---.__(___)
 '''
 
-
+images = [rock, paper, scissors]
 userI = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-if userI == 0:
-  print(rock)
-elif userI == 1:
-  print(paper)
-elif userI == 2:
-  print(scissors)
-
-print("Computer Choose:")
-enemy_choice = random.randint(0,2)
-
-if enemy_choice == 0:
-  print(rock)
-elif enemy_choice == 1:
-  print(paper)
-elif enemy_choice == 2:
-  print(scissors)
-#Logic
-if userI == enemy_choice:
-  print("It's a draw")
-elif userI == 2 and enemy_choice == 1:
-  print("You Win")
-elif userI == 1 and enemy_choice == 0:
-  print("You Win")
-elif userI == 0 and enemy_choice == 2:
-  print("You Win")
+if userI >= 3 or userI < 0:
+  print("Invalid Number, you lose")
 else:
-  print("You Lose")
+  print(images[userI])
+
+  print("Computer Choose:")
+  enemy_choice = random.randint(0,2)
+  print(images[enemy_choice])
+
+  #Logic
+  if userI == enemy_choice:
+    print("It's a draw")
+  elif userI == 2 and enemy_choice == 1:
+    print("You Win")
+  elif userI == 1 and enemy_choice == 0:
+    print("You Win")
+  elif userI == 0 and enemy_choice == 2:
+    print("You Win")
+  else:
+    print("You Lose")
 
 #Rock crushes scissors, scissors cut paper, and paper covers rock
