8b9c90950b21ec5f1bdde6fff1f006808052ab71
James Noel Sacramento <36490528+jncsacramento@users.noreply.github.com>
2022-02-10 22:20:23 +0800

    Update Day 6: Final Project: Escaping the Maze.py

--- Day 6: Final Project: Escaping the Maze.py
+++ Day 6: Final Project: Escaping the Maze.py
@@ -1,5 +1,6 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 #Reborg has it's own sets of functions
+#What you have to do is get the robot out of the maze
 
 
 def turn_right():
