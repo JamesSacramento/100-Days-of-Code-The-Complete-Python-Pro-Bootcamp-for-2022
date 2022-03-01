cae950915b58fc1970adeac85321c1b463fb4a81
James Sacramento <jncsacramento@gmail.com>
2022-03-02 01:56:30 +0800

    -Days Calculator

--- Personal Days Left Counter.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-total_days = 100
-start_date = datetime.date(2022, 2, 3)
-
-
-print(start_date)
---
+++ Personal Project - Days Calculator.py
@@ -0,0 +1,23 @@
+#This is a personal idea project made by me to track:
+# How many days are left and to cram to other days?
+# How many days elapsed?
+
+import datetime
+
+days_100 = 100
+date_today = datetime.date.today()
+start_date = datetime.date(2022, 2, 3)
+days_done = int(input("How many days are done?"))
+
+# Formulas
+days_elapsed = date_today - start_date
+#Time Delta integer converstion
+days_elapsed_int = days_elapsed.days
+#Days Left
+days_left = days_100 - days_elapsed_int
+#Days to be Crammed
+days_crammed = days_elapsed_int - days_done
+
+print(f"Days Elapsed: {days_elapsed_int}")
+print(f"Days Left: {days_left}")
+print(f"Days to be Crammed to other days: {days_crammed}")
